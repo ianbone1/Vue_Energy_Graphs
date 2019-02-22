61bb899be9f320900370c4c6183988277d9af48a
ianbone1 <ianbone1@googlemail.com>
2019-02-22 14:12:01 +0000

    Date Range Stats

--- src/app.js
+++ src/app.js
@@ -12,36 +12,72 @@ document.addEventListener("DOMContentLoaded", () => {
 			generation: [],
 			generationMix: [],
 			chartData: [],
-			dateFrom: null,
-			dateTo: null,
 			selectedChartType: 'PieChart',
 			chartTypes : ["ColumnChart", "PieChart", "BarChart", "LineChart", "AreaChart", "ScatterChart"],
 			chartOptions: {
-          title: 'UK Energy Generation Sources',
-					width: 1000,
-					height: 400,
-					is3D: true
-				}
+				title: 'UK Energy Generation Sources',
+				width: 1000,
+				height: 400,
+				is3D: true
+			},
+			selectedFromDate: null,
+			selectedToDate: null,
+			oneDayAgo: '',
+			twoDaysAgo: '',
+			dateFrom: '',
+			dateTo: ''
 		},
 		methods: {
-			getMix: function(){
-				this.generationMix=this.generation.data.generationmix;
-				this.chartData = [["Fuel", "Percentage"]];
-				for (let fuel of this.generationMix){
-					this.chartData.push([fuel.fuel, fuel.perc]);
+			averageData: function(){
+				const periods = this.generation.data;
+				let data = [];
+				const numPeriods=periods.length;
+				for (let period of periods) {
+					let currentPeriod = period.generationmix
+					for (let fuel of currentPeriod){
+						if (data[fuel.fuel]) {
+							data[fuel.fuel] += fuel.perc;
+						} else {
+							data[fuel.fuel] = fuel.perc;
+						}
+					}
 				}
-				this.dateFrom = new Date(this.generation.data.from);
-				this.dateTo = new Date(this.generation.data.to);
+				let element = ["Fuel", "Percentage"];
+				this.chartData.push(element);
+				for (let d in data){
+					data[d]=(data[d]/numPeriods)
+					element = [d, data[d]]
+					this.chartData.push(element)
+				}
 				this.dataFetched=true;
-				console.log(this.chartData);
-				console.log(this.dateFrom);
-				console.log(this.dateTo);
+			},
+
+			workOutDates: function(){
+				let d = new Date();
+				let mm = d.getMonth() + 1;
+				let dd1 = d.getDate() - 1;
+				let dd2 = d.getDate() - 2;
+				let yy = d.getFullYear();
+				// 2019-02-18
+				// var myDateString = yy + '-' + mm + '-' + dd;
+				this.oneDayAgo = yy + '-' + mm.toString().padStart(2,'0') + '-' + dd1.toString().padStart(2,'0');
+				this.twoDaysAgo = yy + '-' + mm.toString().padStart(2,'0') + '-' + dd2.toString().padStart(2,'0');
+				if (!this.selectedFromDate) { this.selectedFromDate = this.twoDaysAgo;}
+				if (!this.selectedToDate) { this.selectedToDate = this.oneDayAgo;}
+				console.log(d);
+				console.log(this.oneDayAgo);
+				console.log(this.twoDaysAgo);
 			},
 			fetchData: function(){
-				fetch("https://api.carbonintensity.org.uk/generation")
-				  .then(response => response.json())
+				this.workOutDates();
+				this.chartData=[];
+				let url1 = "https://api.carbonintensity.org.uk/generation";
+				let url2 = "https://api.carbonintensity.org.uk/generation" + "/" + this.selectedFromDate + "/" + this.selectedToDate;
+				console.log(url2);
+				fetch(url2)
+					.then(response => response.json())
 					.then(json => this.generation = json)
-					.then(() => this.getMix())
+					.then(() => this.averageData())
 			}
 
 		},
